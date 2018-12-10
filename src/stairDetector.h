1381b8e2ab1879c1ec7aa4fef802497e98968dd9
Winter <39655425+winterworks@users.noreply.github.com>
2018-12-10 12:32:49 +0100

    Fixed the subscribing to MQTT server and used our own server

--- src/stairDetector.h
+++ src/stairDetector.h
@@ -1,39 +1,68 @@
 #include "IQTTFunctions.h"
+#include "config.h"
 
 long passCount = 0;
-int checkDetectorInterval = 5000;
+int checkDetectorInterval = 50;
+int detectingStep = 0;
+int detectingStepSerialCount = 0;
 int infraredDetector = A1;
+int buttonInput = 2;
 void callback(char* topic, byte* payload, unsigned int length);
-MQTT client("iot.eclipse.org", 1883, callback);
 
+MQTT client("m23.cloudmqtt.com", 13776, callback);
+
+// recieve message
 void callback(char* topic, byte* payload, unsigned int length) {
     char p[length + 1];
     memcpy(p, payload, length);
     p[length] = NULL;
 
-    Particle.publish("MQTT receive:", p);
+    Particle.publish("MQTT Passcount:", p);
 }
 
+
 void setup() {
-  // MQTT code from: https://github.com/hirotakaster/MQTT
-  // connect to the server(unique id by Time.now()) and subscribe
-  client.connect("mf_" + String(Time.now()));
-  if (client.isConnected()) {
-      Particle.publish("client connected", "yes");
-      client.subscribe("mf-status");
-  }
+    pinMode(buttonInput, INPUT);
+  
+     Particle.publish("MQTT connection", "Connection to: m23.cloudmqtt.com");
+    client.connect("mf_" + String(Time.now()), username, passoword);
+
+    if (client.isConnected()) {
+        Particle.publish("MQTT connection", "success");
+        client.subscribe("mf-status");
+    } else {
+        Particle.publish("MQTT connection", "failed");
+    }
 }
 
 void loop() {
-  Particle.publish("Input value:", String(analogRead(infraredDetector)));
-  // Publis the detected result to the Particle console
-  bool detecting = analogRead(infraredDetector) > 300;
-  if (client.isConnected()) {
-      client.loop();
-      if (detecting) {
-          passCount++;
-      }
-      client.publish("mf-status", String(passCount));
-  }
-   delay(checkDetectorInterval);
+    if (client.isConnected()) {
+        client.loop();
+    }
+    
+    // bool detecting = analogRead(infraredDetector) < 300;
+    bool detecting = digitalRead(buttonInput);
+    if (detecting) {
+        if (detectingStepSerialCount < 10) {
+            detectingStep++;
+            if(detectingStep > 5){
+                passCount++;
+                detectingStepSerialCount++;
+                // Debugging line for checking the passCount before sending it over MQTT
+                detectingStep = 0;
+                sendValue();
+            }
+        }
+    } else {
+        // Reset the detectingStep
+        detectingStep = 0;
+        detectingStepSerialCount = 0;
+    }
+    delay(checkDetectorInterval);
+}
+
+void sendValue(){
+    if (client.isConnected()) {
+        client.publish("mf-status", String(passCount));
+    }
 }
