bf2f732963904db4f55cd8fcb0e4f9a5b32ad7a8
winterworks <39655425+winterworks@users.noreply.github.com>
2018-12-13 17:00:22 +0100

    Functionality for the photon to sleep during the night

--- src/stairDetector.h
+++ src/stairDetector.h
@@ -1,14 +1,22 @@
 #include "IQTTFunctions.h"
 #include "config.h"
 
+// variables to detect bypassers
+String passCountDay = Time.format(Time.now(), "%Y-%m-%d");
 long passCount = 0;
-int checkDetectorInterval = 50;
+int checkDetectorInterval = 10;
 int detectingStep = 0;
 int detectingStepSerialCount = 0;
+// Used pins
 int infraredDetector = A1;
 int buttonInput = 2;
+// Variable for setting the sleep time and lenght
+int sleepNightSeconds = 60*60*30;
+int bedTimeHour = 23;
+// Callback for MQTT
 void callback(char* topic, byte* payload, unsigned int length);
 
+String topic = "mf-status";
 MQTT client("m23.cloudmqtt.com", 13776, callback);
 
 // recieve message
@@ -20,16 +28,15 @@ void callback(char* topic, byte* payload, unsigned int length) {
     Particle.publish("MQTT Passcount:", p);
 }
 
-
 void setup() {
     pinMode(buttonInput, INPUT);
   
-     Particle.publish("MQTT connection", "Connection to: m23.cloudmqtt.com");
+    Particle.publish("MQTT connection", "Connection to: m23.cloudmqtt.com");
     client.connect("mf_" + String(Time.now()), username, passoword);
 
     if (client.isConnected()) {
         Particle.publish("MQTT connection", "success");
-        client.subscribe("mf-status");
+        client.subscribe(topic);
     } else {
         Particle.publish("MQTT connection", "failed");
     }
@@ -39,18 +46,34 @@ void loop() {
     if (client.isConnected()) {
         client.loop();
     }
-    
+    checkBedTime();
+    detect();
+
+    delay(checkDetectorInterval);
+}
+
+void checkBedTime(){
+    if (Time.format(Time.now(), "%H").toInt() == bedTimeHour) {
+        System.sleep(SLEEP_MODE_DEEP,sleepNightSeconds);
+    }
+    // TODO add an option to also disable the photon in the weekends
+}
+
+void detect(){
     // bool detecting = analogRead(infraredDetector) < 300;
     bool detecting = digitalRead(buttonInput);
     if (detecting) {
-        if (detectingStepSerialCount < 10) {
+        if (detectingStepSerialCount < 5) {
             detectingStep++;
-            if(detectingStep > 5){
-                passCount++;
+            if(detectingStep == 15){
+                increaseLogCount();
                 detectingStepSerialCount++;
                 // Debugging line for checking the passCount before sending it over MQTT
+                publishValue();
+            }
+            if(detectingStep > 30){
+                // Detected long enough restart it
                 detectingStep = 0;
-                sendValue();
             }
         }
     } else {
@@ -58,11 +81,23 @@ void loop() {
         detectingStep = 0;
         detectingStepSerialCount = 0;
     }
-    delay(checkDetectorInterval);
 }
 
-void sendValue(){
+void publishValue(){
     if (client.isConnected()) {
-        client.publish("mf-status", String(passCount));
+        client.publish(topic, String(passCount));
+    }
+}
+
+void increaseLogCount(){
+    if (passCountDay == Time.format(Time.now(), "%Y-%m-%d")) {
+        // It's stil the same day increase the current count
+        passCount++;
+        
+    } else {
+        // The day has passed reset the count
+        passCount = 0;
+        passCountDay = Time.format(Time.now(), "%Y-%m-%d");
+        // TODO make a log of the past days
     }
 }
